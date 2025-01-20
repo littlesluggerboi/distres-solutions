dbd2ee58eda1d42ff985b1451231af22f86f5ac0
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-01-20 23:10:35 +0800

    Added a makeshift data source for blogs page.

---
+++ src/data/blogs_data.js
@@ -0,0 +1,87 @@
+const data = [
+  {
+    title: "Categories of Hazardous Wastes",
+    type: "blog",
+    paragraphs: [
+      " Hazardous wastes are any substance that poses risk to the environment and to the health and safety of individuals. These wastes have characteristics that make them hazardous, namely: ignitable, corrosive, reactive or toxic. Under the Procedural Manual Title II of DAO 92-29 “Hazardous Waste Management”, the Department of Environment And Natural Resources have classified hazardous wastes into 13 categories.",
+      `Wastes with cyanide. Liquid wastes with a cyanide concentration greater than 200 ppm are under this category. Cyanide is widely used in the metal-finishing industry to assure the quality of finished products. It is also used for extracting gold from ores and turning it into liquid.
+      Acid wastes are corrosive in nature and have a pH of less than or equal to 2. These types of wastes come from laboratories and coal mines and include chemicals such as sulfuric acid, hydrochloric acid, nitric acid and any organic acid.
+Alkali wastes are chemicals with a pH level of greater than or equal to 12.5. One of the most common uses of alkali is for making soaps and detergents. Examples of chemicals that are under this category are caustic soda, potash and alkaline cleaners.
+Wastes with inorganic chemicals. Includes chemicals such as selenium, arsenic, barium, chromium, lead, and cadmium. Arsenic and chromium are industrially used as alloying agents which improves properties of steels such as corrosion resistance and strength.`,
+      `Reactive chemical wastes. Includes wastes that react violently and can form explosive mixtures with water. Explosive and unstable chemicals are also part of this category.
+Inks/Dyes/Pigments/Pain/Latex/Adhesives/Organic Sludge. These substances are detrimental to a person’s health. So, wastes derived from them are to be managed accordingly.
+Waste organic solvent. Organic solvents have two types: halogenated organic solvents and non halogenated organic solvents. They dissolve certain materials and substances and are used in the process of manufacturing pharmaceutical products, paints and adhesives.`,
+      `Putrescible/ Organic Wastes. Putrescible are wastes that come from facilities such as poultry farms and slaughterhouses. This includes animal wastes and grease trap wastes.
+Oil. Any waste that comes from establishments that generate transport or treat more than 200L of waste oil per day, not including vegetable oil and waste tallow.
+Containers that are used to hold other hazardous wastes are also subject to treatment and management.
+Immobilized Wastes. These are wastes that are not likely to be removed through vaporization. They are connected through chemical bonds.
+`,
+      ` Organic Chemicals. Polychlorinated biphenyl or PCB is one of the hazardous waste under this category. It is mostly found in electrical equipment such as transformers and capacitors and is highly carcinogenic. Other wastes under this category include Chlorofluorocarbons(CFC) and halons.
+Miscellaneous Wastes. Expired drugs and other pharmaceutical wastes are part of this group. Pathological or infectious wastes such as body fluids, blood, used needles and tissues are also included.`,
+    ],
+  },
+  {
+    title: "Categories of TSD Facilities",
+    type: "blog",
+    paragraphs: [
+      `Treatment, Storage, and Disposal (TSD) facilities are responsible for the, as the name implies, treatment, storage and disposal of hazardous wastes. The Environmental Management Bureau (EMB) has categorized these facilities by how they manage or method of treatment of hazardous wastes.`,
+      `Category A - Onsite Treatment and Disposal Facilities. As implied, Category A facilities conduct management of hazardous waste generated onsite or within the facility. These facilities have the function or technologies from Categories B to E and G but do not offer them for other establishments. Examples of entities that have onsite facilities are the Lepanto and Philex Mining in the Cordillera Region.
+    Category B - Thermal Treatment Facilities. Thermal Treatment Facilities have equipment that implement methods of using heat to treat hazardous wastes. This includes pyrolysis, autoclave, thermolysis and thermal evaporation. Each method has specific categories of hazardous wastes it can accommodate. At the same time, each equipment that implements such methods are subject to specifications from the EMB.
+    `,
+      `Category C - Disposal Facilities. Disposal Facilities take in hazardous waste that have undergone treatment and are for final disposal. They are subcategorized into two groups: a) Sanitary Landfills which accepts non-liquid hazardous wastes for final disposal and b) Surface impoundments meant for liquid counterparts.
+    Category D - Recycling Facilities. Recycling facilities perform the following methods for treating wastes: a) burning hazardous wastes and using it as alternative fuel, b) landfilling, backfilling, and composting, and c) reclamation or the process of reprocessing hazardous waste or removing contaminants to restore the waste to a usable condition.`,
+      `Category E - Chemical Treatment Facilities. Chemical Treatment facilities use chemical treatment methods namely, chemical immobilization, polymeric filtration, chemical decomposition and solvent extraction to treat hazardous wastes that are generated outside of the facility. Category F - Storage Facilities. Storage facilities are where hazardous wastes that are waiting to be treated, transported, disposed or exported are stored. They are made of concrete and are completely enclosed to prevent the possible leakage of the waste. 
+    Category G - Facilities that Decommission PCB-containing Equipment. These facilities are specialized to accept polychlorinated biphenyl (PCB)-containing equipment such as transformers and capacitors. PCBs are either drained or dismantled from PCB-containing equipment in preparation for storage, treatment, and/or disposal.`,
+      `As of January 2022, a total of 165 TSD facilities are registered through the EMB. Even with this number, the country still cannot manage all of the wastes generated by the country. This became even more evident during the pandemic as problems arose due to the lack of TSD facilities that can cater the huge amount of health care wastes generated from fighting against the coronavirus.`,
+    ],
+  },
+  {
+    title: "The Importance of Hazardous Waste Management",
+    type: "blog",
+    paragraphs: [
+      `The disposal of wastes has been one of the biggest problems our country has been facing for the past years. The number of treatment facilities have not been able to keep up with the amount of waste the country has been generating. Due to the lack in numbers, most of the wastes are transported to a different province or region that have the capacity for waste management. One of the reasons for the deficiency of treatment facilities can also be attributed to the cost for making and maintaining one. But despite the cost and inconveniences presented by waste management, its implementation is still very important for a number of reasons.`,
+      `One of the primary reasons for managing hazardous waste is because hazardous wastes pollutes the environment. Improper disposal of waste can lead to water pollution which results in the worsening of the quality of water and endangers marine biodiversity. Oil spillage is an example of how hazardous wastes can lead to loss of marine life. When coral reefs come in contact with oil, it can die or suffer impediment of reproduction, growth and development which in turn affects many species of fish, crabs and other marine lives living within them.
+    `,
+      `Land-pollution is another detrimental effect of introduction of hazardous wastes to the environment. When land gets polluted by hazardous wastes, the waste substance sips through the land and contaminates it together with the water underneath. Due to this, groundwater which is one of our sources of drinkable water is put into risk. At the same time, the land contaminated by hazardous waste will become unable to support wildlife.
+    `,
+      `Hazardous waste management is not only important for the safety of the environment but also for public health. Some hazardous wastes are mutagenic and/or carcinogenic. Carcinogens are substances that can cause cancer while mutagens cause damage to the genetic material which can lead to mutation and ultimately result in diseases such as birth defects and genetic mutation. This is one of the reasons why contaminated lands become unusable for human settlement.`,
+      `But other than these reasons, hazardous waste management also includes the process of recycling hazardous wastes and using them as alternatives for fuel.
+    `,
+    ],
+  },
+  {
+    title: "Frequently Asked Questions",
+    type: "faq",
+    faq: [
+      {
+        question: "A. What locations can your services reach?",
+        answer:
+          "DiStreS Solutions provides services for establishments, institutions and individuals within the region and neighboring provinces.",
+      },
+      {
+        question:
+          "B. Is it possible to request a refund or cancel the service?",
+        answer:
+          "Before signing the legal documents, refund or cancellation of service will still be available. If after the signing of legal documents, the customer decided not to push through with the transaction, only 80% of the original payment will be refunded.",
+      },
+      {
+        question: "C. How will I know that the company is credible?",
+        answer:
+          "The company is bound by contract when undergoing transactions and agreements. This guarantees credibility and integrity when transacting with the company. On the other hand, the facilities and equipment are all accredited by the government ensuring its quality and functionality.",
+      },
+    ],
+  },
+  {
+    title: "References",
+    type: "blog",
+    paragraphs: [
+      "https://unsplash.com/@kylebushnell?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      "https://unsplash.com/@john_cameron?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      "https://www.freepik.com/free-photo/close-up-researcher-holding-glassware_11630658.htm#query=laboratory&position=0&from_view=search&track=sp",
+      "https://www.freepik.com/free-vector/social-media-logos-icons-set_11387944.htm#query=WEBSITE%20ICONS&position=1&from_view=search&track=sp",
+      "https://www.freepik.com/free-photo/aerial-view-gas-oil-refinery-oil-industry_23404841.htm#query=oil%20containers&position=7&from_view=search&track=sph",
+    ],
+  },
+];
+
+export default data;
